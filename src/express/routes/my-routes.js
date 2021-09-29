ef7b310f597dce54bb5a332de2b70d861bffc440
Lidaaaaa <89680020+Lidaaaaa@users.noreply.github.com>
2021-09-29 22:47:19 +0500

    module3-task2 (#2)

--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -3,7 +3,7 @@
 const {Router} = require(`express`);
 const offersRouter = new Router();
 
-offersRouter.get(`/`, (req, res) => res.send(`/my`));
-offersRouter.get(`/comments`, (req, res) => res.send(`/my/comments`));
+offersRouter.get(`/`, (req, res) => res.render(`my/my-tickets`));
+offersRouter.get(`/comments`, (req, res) => res.render(`my/comments`));
 
 module.exports = offersRouter;
