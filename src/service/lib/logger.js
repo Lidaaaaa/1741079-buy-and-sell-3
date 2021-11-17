1dea432b65b44a42a0569c2a8906f003950d9d4a
Lidaaaaa <89680020+Lidaaaaa@users.noreply.github.com>
2021-11-17 10:22:50 +0500

    module4-task2 fix pino-logger (#10)

--- src/service/lib/logger.js
+++ src/service/lib/logger.js
@@ -1,19 +1,26 @@
 "use strict";
 
 const pino = require(`pino`);
-const {Env} = require(`../../constants`);
-const LOG_FILE = `./logs/api.log`;
 
-const isDevMode = process.env.NODE_ENV === Env.DEVELOPMENT;
-const logger = pino({
-  name: `base-logger`,
-  level: process.env.LOG_LEVEL || (isDevMode ? `info` : `error`),
+const {Env} = require(`../../constants`);
+
+const LOG_FILE = `./logs/api.log`;
+const isProdMode = process.env.NODE_ENV === Env.PRODUCTION;
+const logLevel = process.env.LOG_LEVEL || (isProdMode ? `error` : `info`);
+
+const prodOptions = {
   transport: {
-    target: isDevMode ? `pino-pretty` : `pino/file`,
+    target: `pino/file`,
     options: {
-      destination: isDevMode ? process.stdout : LOG_FILE
+      destination: LOG_FILE
     }
   }
+};
+
+const logger = pino({
+  name: `base-logger`,
+  level: logLevel,
+  ...(isProdMode && prodOptions)
 });
 
 module.exports = {
