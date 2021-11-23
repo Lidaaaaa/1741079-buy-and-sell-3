956aba764a48fa1674242e0b63e00d2d02a67a75
Lidaaaaa <89680020+Lidaaaaa@users.noreply.github.com>
2021-11-23 22:04:56 +0500

    module4-task3 (#11)

--- src/express/express.js
+++ src/express/express.js
@@ -2,25 +2,30 @@
 
 const express = require(`express`);
 const path = require(`path`);
+
 const offersRoutes = require(`./routes/offers-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
+const {HttpCode, DEFAULT_FRONT_SERVER_PORT} = require(`../constants`);
 
-const DEFAULT_PORT = 8080;
+const port = process.env.PORT || DEFAULT_FRONT_SERVER_PORT;
 const PUBLIC_DIR = `public`;
 const TEMPLATE_DIR = `templates`;
+const UPLOAD_DIR = `upload`;
 
 const app = express();
+
+app.use(`/`, mainRoutes);
+app.use(`/my`, myRoutes);
+app.use(`/offers`, offersRoutes);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+app.use(express.static(path.resolve(__dirname, UPLOAD_DIR)));
+
+app.use((_req, res) => res.status(HttpCode.BAD_REQUEST).render(`errors/404`));
+app.use((_err, _req, res) => res.status(HttpCode.INTERNAL_SERVER_ERROR).render(`errors/500`));
 
 app.set(`views`, path.resolve(__dirname, TEMPLATE_DIR));
 app.set(`view engine`, `pug`);
 
-app.use(`/offers`, offersRoutes);
-app.use(`/my`, myRoutes);
-app.use(`/`, mainRoutes);
-
-app.use((req, res) => res.status(400).render(`errors/404`));
-app.use((err, req, res) => res.status(500).render(`errors/500`));
-
-app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
-app.listen(DEFAULT_PORT);
+app.listen(port);
--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -1,9 +1,19 @@
 "use strict";
 
 const {Router} = require(`express`);
-const offersRouter = new Router();
+const {getAPI} = require(`../api`);
 
-offersRouter.get(`/`, (req, res) => res.render(`my/my-tickets`));
-offersRouter.get(`/comments`, (req, res) => res.render(`my/comments`));
+const offersRouter = new Router();
+const api = getAPI();
+
+offersRouter.get(`/`, async (_req, res) => {
+  const offers = await api.getOffers();
+  res.render(`my/my-tickets`, {offers});
+});
+
+offersRouter.get(`/comments`, async (_req, res) => {
+  const offers = await api.getOffers();
+  res.render(`my/comments`, {offers: offers.slice(0, 5)});
+});
 
 module.exports = offersRouter;
