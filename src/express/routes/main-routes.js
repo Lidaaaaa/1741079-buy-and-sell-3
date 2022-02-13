61b59e11c64e86c5f508529c252793205c0fef8c
Lidaaaaa <89680020+Lidaaaaa@users.noreply.github.com>
2022-02-13 17:01:03 +0500

    module7-task3 (#18)
    * module7-task3

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -11,6 +11,7 @@ const api = getAPI();
 const OFFERS_PER_PAGE = 8;
 
 mainRouter.get(`/`, async (req, res) => {
+  const {user} = req.session;
   let {page = 1} = req.query;
   page = +page;
 
@@ -22,10 +23,24 @@ mainRouter.get(`/`, async (req, res) => {
   ]);
 
   const totalPage = Math.ceil(count / OFFERS_PER_PAGE);
-  res.render(`main/main`, {offers, categories, totalPage, page});
+  res.render(`main/main`, {
+    offers,
+    categories,
+    totalPage,
+    page,
+    user
+  });
 });
 
-mainRouter.get(`/register`, (_req, res) => res.render(`main/sign-up`));
+mainRouter.get(`/register`, (req, res) => {
+  const {user} = req.session;
+
+  if (user) {
+    return res.redirect(`/`);
+  }
+
+  return res.render(`main/sign-up`);
+});
 
 mainRouter.post(`/register`, upload.single(`avatar`), async (req, res) => {
   const {body, file} = req;
@@ -38,27 +53,59 @@ mainRouter.post(`/register`, upload.single(`avatar`), async (req, res) => {
     passwordRepeated: body[`user-password-again`]
   };
 
-  console.log(userData);
-
   try {
     await api.createUser(userData);
     res.render(`main/login`);
   } catch (e) {
     const validationMessages = prepareErrors(e);
-    console.log(e);
-    res.render(`main/sign-up`, {validationMessages});
+    const {user} = req.session;
+    res.render(`main/sign-up`, {
+      validationMessages,
+      user
+    });
   }
 });
 
-mainRouter.get(`/login`, (_req, res) => res.render(`main/login`));
+mainRouter.get(`/login`, (req, res) => {
+  const {user} = req.session;
+
+  if (user) {
+    return res.redirect(`/`);
+  }
+
+  return res.render(`main/login`);
+});
+
+mainRouter.post(`/login`, async (req, res) => {
+  const data = {
+    email: req.body[`user-email`],
+    password: req.body[`user-password`]
+  };
+
+  try {
+    const user = await api.auth(data);
+    req.session.user = user;
+    req.session.save(() => res.redirect(`/`));
+  } catch (e) {
+    const validationMessages = prepareErrors(e);
+    const user = req.session;
+    res.render(`login`, {user, validationMessages});
+  }
+});
+
+mainRouter.get(`/logout`, (req, res) => {
+  delete req.session.user;
+  res.redirect(`/`);
+});
 
 mainRouter.get(`/search`, async (req, res) => {
+  const {user} = req.session;
+  const {query} = req.query;
   try {
-    const {query} = req.query;
     const results = await api.search(query);
-    res.render(`main/search-result`, {results});
+    res.render(`main/search-result`, {results, user});
   } catch (e) {
-    res.render(`main/search-result`, {results: []});
+    res.render(`main/search-result`, {results: [], user});
   }
 });
 
--- src/express/routes/offers-routes.js
+++ src/express/routes/offers-routes.js
@@ -1,13 +1,16 @@
 "use strict";
 
 const {Router} = require(`express`);
+const csrf = require(`csurf`);
 
 const {getAPI} = require(`../api`);
 const {ensureArray, prepareErrors} = require(`../../utils`);
 const upload = require(`../middlewares/upload`);
+const auth = require(`../middlewares/auth`);
 
 const offersRouter = new Router();
 const api = getAPI();
+const csrfProtection = csrf();
 
 const getAddOfferData = () => {
   return api.getCategories();
@@ -22,20 +25,32 @@ const getViewOfferData = (offerId, comments) => {
   return api.getOffer(offerId, comments);
 };
 
-offersRouter.get(`/category/:id`, (_req, res) => res.render(`offers/category`));
+offersRouter.get(`/category/:id`, (req, res) => {
+  const {user} = req.session;
 
-offersRouter.get(`/add`, async (_req, res) => {
+  res.render(`offers/category`, {user});
+});
+
+offersRouter.get(`/add`, auth, csrfProtection, async (req, res) => {
+  const {user} = req.session;
   const categories = await api.getCategories();
-  res.render(`offers/ticket-add`, {categories});
+
+  res.render(`offers/ticket-add`, {
+    categories,
+    user,
+    csrfToken: req.csrfToken()
+  });
 });
 
 offersRouter.get(`/:id`, async (req, res) => {
+  const {user} = req.session;
   const {id} = req.params;
   const offer = await getViewOfferData(id, true);
-  res.render(`offers/ticket`, {offer, id});
+
+  res.render(`offers/ticket`, {offer, id, user});
 });
 
-offersRouter.post(`/add`, upload.single(`avatar`), async (req, res) => {
+offersRouter.post(`/add`, auth, csrfProtection, upload.single(`avatar`), async (req, res) => {
   const {body, file} = req;
 
   const offerData = {
@@ -52,21 +67,31 @@ offersRouter.post(`/add`, upload.single(`avatar`), async (req, res) => {
     res.redirect(`my/my-tickets`);
   } catch (e) {
     const validationMessages = prepareErrors(e);
+    const {user} = req.session;
     const categories = await getAddOfferData();
+
     res.render(`offers/ticket-add`, {
       categories,
-      validationMessages
+      validationMessages,
+      user
     });
   }
 });
 
-offersRouter.get(`/edit/:id`, async (req, res) => {
+offersRouter.get(`/edit/:id`, auth, csrfProtection, async (req, res) => {
+  const {user} = req.session;
   const {id} = req.params;
   const [offer, categories] = await getEditOfferData(id);
-  res.render(`offers/ticket-edit`, {offer, categories});
+
+  res.render(`offers/ticket-edit`, {
+    offer,
+    categories,
+    user,
+    csrfToken: req.csrfToken()
+  });
 });
 
-offersRouter.post(`/edit/:id`, async (req, res) => {
+offersRouter.post(`/edit/:id`, auth, csrfProtection, async (req, res) => {
   const {body, file} = req;
   const {id} = req.params;
   const offerData = {
@@ -83,8 +108,16 @@ offersRouter.post(`/edit/:id`, async (req, res) => {
     res.redirect(`/my`);
   } catch (e) {
     const validationMessages = prepareErrors(e);
+    const {user} = req.session;
     const [offer, categories] = await getEditOfferData(id);
-    res.render(`offers/ticket-edit`, {id, offer, categories, validationMessages});
+
+    res.render(`offers/ticket-edit`, {
+      id,
+      offer,
+      categories,
+      user,
+      validationMessages
+    });
   }
 });
 
@@ -97,8 +130,15 @@ offersRouter.post(`/:id/comments`, async (req, res) => {
     res.redirect(`/offers/${id}`);
   } catch (e) {
     const validationMessages = prepareErrors(e);
+    const {user} = req.session;
     const offer = await getViewOfferData(id, true);
-    res.render(`offers/ticket`, {offer, id, validationMessages});
+
+    res.render(`offers/ticket`, {
+      offer,
+      id,
+      validationMessages,
+      user
+    });
   }
 });
 
